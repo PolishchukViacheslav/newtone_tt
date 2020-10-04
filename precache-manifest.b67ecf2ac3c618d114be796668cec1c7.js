5840f40e06a2b86c35bc90d4e0255de7580747ed
Slava Polishchuk <dizerllc@gmail.com>
2020-10-04 16:20:49 +0300

    Updates

---
+++ precache-manifest.b67ecf2ac3c618d114be796668cec1c7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc4a1215ee8d874b1feb6f1c0017971f",
+    "url": "/newtone_tt/index.html"
+  },
+  {
+    "revision": "17f38cd7c0b260979e86",
+    "url": "/newtone_tt/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "821bcba121dd07ad8672",
+    "url": "/newtone_tt/static/css/main.207506de.chunk.css"
+  },
+  {
+    "revision": "17f38cd7c0b260979e86",
+    "url": "/newtone_tt/static/js/2.c6614629.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/newtone_tt/static/js/2.c6614629.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "821bcba121dd07ad8672",
+    "url": "/newtone_tt/static/js/main.56ee46f8.chunk.js"
+  },
+  {
+    "revision": "3351c09df54b3e6fb7fc",
+    "url": "/newtone_tt/static/js/runtime-main.27695527.js"
+  },
+  {
+    "revision": "4f75bc569252b02a45ca3ae4a7b6d6a3",
+    "url": "/newtone_tt/static/media/cart.4f75bc56.svg"
+  },
+  {
+    "revision": "90f46705924ef47731fd1dee4ff2754a",
+    "url": "/newtone_tt/static/media/pay.90f46705.svg"
+  }
+]);
